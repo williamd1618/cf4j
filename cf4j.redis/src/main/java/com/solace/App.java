b08101eff94cf951327c2b276199d9931d44bed9
Daniel Williams <daniel.williams@gmail.com>
2014-07-02 16:20:27 -0600

    initial project sturcture

---
+++ cf4j.redis/src/main/java/com/solace/App.java
@@ -0,0 +1,13 @@
+package com.solace;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
